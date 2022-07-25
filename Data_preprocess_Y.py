218c9d8fbc3dc911d95a7b76559d4010a322da7d
orDrink <92385297+orDrink@users.noreply.github.com>
2022-07-25 16:44:20 -0400

    Update Data_preprocess_Y.py

--- Data_preprocess_Y.py
+++ Data_preprocess_Y.py
@@ -6,7 +6,9 @@ import numpy as np
 from numpy import *
 from matplotlib import pyplot as plt
 
-Yfile=["C","I","B","C_test","I_test","B_test"] # Files need to be processed 
+# Files need to be processed 
+Yfile=["C_SOC00","I_SOC00","B_SOC00","C_SOC00_test","I_SOC00_test","B_SOC00_test","C_SOC60","I_SOC60","B_SOC60","C_SOC60_test","I_SOC60_test","B_SOC60_test"]
+Yfile_E=["C_SOC00_E","I_SOC00_E","B_SOC00_E","C_SOC00_E_test","I_SOC00_E_test","B_SOC00_E_test","C_SOC60_E","I_SOC60_E","B_SOC60_E","C_SOC60_E_test","I_SOC60_E_test","B_SOC60_E_test"]
 
 def Smin_max(a):
     if len(a) == 1:
@@ -20,17 +22,16 @@ def Smin_max(a):
     rmin, rmax = Smin_max(a[m:])
     return min(lmin, rmin), max(lmax, rmax)
 
+k = 0
+
 for filename in Yfile:
     curvefile = open(filename+"_curve.csv", "r") # This file save simulation force - displacement curve 
     peakfile = open(filename+"_peak.csv", "r") # This file save ISC triggering forces from repeat tests
-    pfile = open(filename+"_Y.csv", "w",encoding='utf-8',newline='')
-
-# stdf = 0.02464
+    pfile = open(Yfile_E[k]+"_Y.csv", "w",encoding='utf-8',newline='')
 
     reader_peakfile = csv.reader(peakfile)
     reader_curvefile = csv.reader(curvefile)
 
-# x1 = np.arange(5).reshape((5, 1))
     peak = []
     curve = []
 
@@ -57,18 +58,14 @@ for filename in Yfile:
                 displacement[i] = (array_peak[i] - array_curve[lm-1][1]) * (array_curve[lm][0] - array_curve[lm-1][0]) / (
                             array_curve[lm][1] - array_curve[lm-1][1]) + array_curve[lm-1][0]
 
-# print(displacement[i])
-
     d_mean = mean(displacement)
-# print(d_mean)
     d_std = np.std(displacement,ddof=1)
-# d_std = stdf * d_mean
-# print(d_std/d_mean)
 
     pd = np.zeros(len(array_curve),dtype=float)
     p = np.zeros(len(array_curve),dtype=float)
 
     step = 0.05
+    # step = 0.05 CHANGED
     p0 = 0
 
     writer = csv.writer(pfile)
@@ -78,16 +75,20 @@ for filename in Yfile:
         if i==0:
             p[i] = p0
         else:
-            p0 = p0 + (pd[i-1]+pd[i])/2 * step
+            p0 = p0 + (pd[i-1]+pd[i])/2 * (array_curve[i][0]-array_curve[i-1][0])
+            # p0 = p0 + (pd[i - 1] + pd[i]) / 2 * step
             p[i]= p0
-            if p[i] > 0.999999: p[i]=1
+            if p[i] > 0.999999: p[i] = 1
             if p[i] <=0.000001: p[i] = 0
         writer.writerow([p[i]])
+    
     # print(i, p[i])
 
     curvefile.close()
     peakfile.close()
     pfile.close()
+
+    k = k + 1
 
 
 # x = np.linspace(0,len(array_curve),len(array_curve))
@@ -96,13 +97,3 @@ for filename in Yfile:
 # fig=plt.figure(figsize=(150,6))
 # plt.plot(x, y,label='First Curve')
 # plt.show()
-
-
-
-
-
-
-
-
-
-
