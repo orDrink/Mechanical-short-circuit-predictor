bba987a7d4760e00a8edc94014c2185e0347786c
orDrink <92385297+orDrink@users.noreply.github.com>
2022-07-25 15:56:47 -0400

    Add files via upload

---
+++ preprocess_Y_V2.py
@@ -0,0 +1,106 @@
+import math
+import csv
+import numpy as np
+from numpy import *
+from matplotlib import pyplot as plt
+
+Yfile=["C","I","B","C_test","I_test","B_test"]
+
+def Smin_max(a):
+    if len(a) == 1:
+        return (a[0], a[0])
+
+    elif len(a) == 2:
+        return (min(a), max(a))
+
+    m = len(a) // 2
+    lmin, lmax = Smin_max(a[:m])
+    rmin, rmax = Smin_max(a[m:])
+    return min(lmin, rmin), max(lmax, rmax)
+
+for filename in Yfile:
+    curvefile = open(filename+"_curve.csv", "r")
+    peakfile = open(filename+"_peak.csv", "r")
+    pfile = open(filename+"_Y.csv", "w",encoding='utf-8',newline='')
+
+# stdf = 0.02464
+
+    reader_peakfile = csv.reader(peakfile)
+    reader_curvefile = csv.reader(curvefile)
+
+# x1 = np.arange(5).reshape((5, 1))
+    peak = []
+    curve = []
+
+    for item in reader_peakfile:
+        peak.append(float(item[0]))
+        # print(item[0])
+
+    for item in reader_curvefile:
+        curve.append([float(item[0]),float(item[1])])
+        # print(item[0],item[1])
+
+    array_peak = np.array(peak)
+    array_curve = np.array(curve)
+
+    displacement = np.zeros(len(array_peak),dtype=float)
+
+    for i in range(len(array_peak)):
+        for j in range(len(array_curve)-1):
+            if (array_curve[j][1]<array_peak[i]) & (array_curve[j+1][1]>=array_peak[i]):
+                displacement[i] = (array_peak[i] - array_curve[j][1]) * (array_curve[j + 1][0] - array_curve[j][0]) / (
+                            array_curve[j + 1][1] - array_curve[j][1]) + array_curve[j][0]
+            if (array_curve[len(array_curve)-1][1]<array_peak[i]):
+                lm=len(array_curve)-1
+                displacement[i] = (array_peak[i] - array_curve[lm-1][1]) * (array_curve[lm][0] - array_curve[lm-1][0]) / (
+                            array_curve[lm][1] - array_curve[lm-1][1]) + array_curve[lm-1][0]
+
+# print(displacement[i])
+
+    d_mean = mean(displacement)
+# print(d_mean)
+    d_std = np.std(displacement,ddof=1)
+# d_std = stdf * d_mean
+# print(d_std/d_mean)
+
+    pd = np.zeros(len(array_curve),dtype=float)
+    p = np.zeros(len(array_curve),dtype=float)
+
+    step = 0.05
+    p0 = 0
+
+    writer = csv.writer(pfile)
+
+    for i in range(len(array_curve)):
+        pd[i] = math.exp(-math.pow(array_curve[i][0]-d_mean,2)/(2*d_std*d_std)) /(d_std * math.pow(2*3.14159,0.5))
+        if i==0:
+            p[i] = p0
+        else:
+            p0 = p0 + (pd[i-1]+pd[i])/2 * step
+            p[i]= p0
+            if p[i] > 0.999999: p[i]=1
+            if p[i] <=0.000001: p[i] = 0
+        writer.writerow([p[i]])
+    # print(i, p[i])
+
+    curvefile.close()
+    peakfile.close()
+    pfile.close()
+
+
+# x = np.linspace(0,len(array_curve),len(array_curve))
+# y = p
+#
+# fig=plt.figure(figsize=(150,6))
+# plt.plot(x, y,label='First Curve')
+# plt.show()
+
+
+
+
+
+
+
+
+
+
---
+++ training_test_V3.py
@@ -0,0 +1,152 @@
+
+import math
+import csv
+import numpy as np
+from numpy import *
+from sklearn.svm import SVR
+from matplotlib import pyplot as plt
+from scipy.stats import pearsonr
+from sklearn.metrics import r2_score
+import time
+from sklearn import preprocessing
+
+filename = ["C","I","B"]
+filename_test=["C_test_","I_test_","B_test_"]
+
+testsample = np.array([141,181,181])
+
+
+# testsample = np.array([161])
+# filename_test=["I_test_"]
+
+
+# open files
+p = []
+N = np.zeros([len(filename)])
+
+i = 0
+for name in filename:
+    pfile = open(name+"_Y.csv", "r")
+    reader_pfile = csv.reader(pfile)
+    for item in reader_pfile:
+        y_temp = float(item[0])
+        if y_temp < 0.001 : y_temp = 0.001
+        if y_temp > 0.999 : y_temp = 0.999
+        Y_linear = -math.log(1/y_temp-1)
+        p.append(Y_linear)
+        N[i] = N[i] + 1
+    i = i + 1
+    pfile.close()
+Y = np.array(p)
+
+print("training sample", int(sum(N)))
+
+i = 0
+d = 11
+X = np.zeros([int(sum(N)), d * d * d * d])
+n = 0
+
+for name in filename:
+    j = 0
+    while (j < N[i]):
+        sfile = open(name + str(j + 1) + ".csv", "r")
+        reader_sfile = csv.reader(sfile)
+        for line in reader_sfile:
+            x1 = int(float(line[0]) * 10)
+            x2 = int(float(line[1]) * 10)
+            x3 = int(float(line[2]) * 10)
+            x4 = int(float(line[3]) * 10)
+            position = x1 + d * x2 + d * d * x3 + d * d * d * x4
+            if (position < d * d * d * d):
+                X[n][position] = 1
+        sfile.close()
+        j = j + 1
+        n = n + 1
+    i = i + 1
+
+# C_range = np.logspace(-2, 10, 13)
+# gamma_range = np.logspace(-9, 3, 13)
+# param_grid = dict(gamma=gamma_range, C=C_range)
+
+
+svr = SVR(kernel='rbf', C=100, gamma=0.001, epsilon=0.0001)
+# svr = SVR(kernel='sigmoid', gamma=0.001, coef0=0.001)
+t0 = time.time()
+regr = svr.fit(X, Y)
+print(time.time() - t0, "seconds process time")
+
+i = 0
+for name in filename_test:
+    Test_sample = testsample[i]
+    testfile = name
+    print(name)
+    Xp = np.zeros([Test_sample, d * d * d * d])
+    j = 0
+    while (j < Test_sample):
+        sfile = open(testfile + str(j + 1) + ".csv", "r")
+        reader_sfile = csv.reader(sfile)
+        for line in reader_sfile:
+            x1 = int(float(line[0]) * 10)
+            x2 = int(float(line[1]) * 10)
+            x3 = int(float(line[2]) * 10)
+            x4 = int(float(line[3]) * 10)
+            position = x1 + d * x2 + d * d * x3 + d * d * d * x4
+            if (position < d * d * d * d):
+                Xp[j][position] = 1
+        sfile.close()
+        j = j + 1
+
+    pt = []
+    ofile = open(name + "Y.csv", "r")
+    reader_ofile = csv.reader(ofile)
+    for item in reader_ofile:
+        pt.append(float(item[0]))
+    ofile.close()
+
+    x1 = np.linspace(0, len(Xp)-1, len(Xp))
+    y1 = svr.fit(X, Y).predict(Xp)
+
+    for k in range(len(Xp)):
+        y1[k] = 1/(1+math.exp(-y1[k]))
+        # if y1[k]>1: y1[k] = 1
+        # if y1[k]<0: y1[k] = 0
+
+    predictfile = open(filename_test[i]+".csv", "w", encoding='utf-8', newline='')
+    writer = csv.writer(predictfile)
+    for k in range(len(Xp)-1):
+        # if y1[k]>1: y1[k] = 1
+        # if y1[k]<0: y1[k] = 0
+        x1t = str(x1[k]*0.0005)
+        x2t = str(y1[k])
+        x3t = str(pt[k])
+        writer.writerow([x1t, x2t, x3t])
+    predictfile.close()
+
+
+    x1f = np.linspace(0, (len(Xp) - 1) * 0.05, len(Xp))
+    y1f = y1
+
+    x2 = np.linspace(0, (len(pt)-1) * 0.05, len(pt))
+    y2 = np.array(pt)
+    plt.plot(x1f, y1f, label=name)
+    plt.scatter(x2, y2, label=name)
+
+    i = i + 1
+
+Y = np.array(p)
+
+
+plt.show()
+
+# print(r2_score(y1, y2))
+
+
+
+
+
+
+
+
+
+
+
